608ec5c2810e7ceb47422c68e6be22dc3463b6b4
Akiva <breuerakiva@gmail.com>
2023-06-02 02:23:38 +0300

    ai is working with basic ui.

--- script.js
+++ script.js
@@ -478,7 +478,6 @@
 //   };
 // })();
 
-
 // const gameFlow = (()=>{
 //   let bestMove;
 
@@ -490,7 +489,7 @@
 //       aiPlay();
 //     }
 //   };
-  
+
 //   const aiPlay=()=>{
 //     let checkGame = evaluate()
 //     if (checkGame === 1){
@@ -543,13 +542,13 @@
 //     const winningCombos = [
 //       [[0,0],[0,1],[0,2]],//rows
 //       [[1,0],[1,1],[1,2]],
-//       [[2,0],[2,1],[2,2]], 
+//       [[2,0],[2,1],[2,2]],
 //       [[0,0],[1,0],[2,0]],//col
 //       [[0,1],[1,1],[2,1]],
-//       [[0,2],[1,2],[2,2]], 
+//       [[0,2],[1,2],[2,2]],
 //       [[0,0],[1,1],[2,2]],//diagonal
-//       [[0,2],[1,1],[2,0]]] 
-  
+//       [[0,2],[1,1],[2,0]]]
+
 //     // Check for a winner
 //     for (let combo of winningCombos) {
 //       const [a, b, c] = combo;
@@ -564,15 +563,15 @@
 //           return 1; // AI wins
 //       }
 //     }
-  
+
 //     // Game is a draw
 //     if (isBoardFull(board))
 //       return 0;
-  
+
 //     // No winner yet
 //     return null;
 //   }
-  
+
 //   const isBoardFull = (board)=> {
 //     for (let i = 0; i<3; i++) {
 //       for (let j = 0; j<3; j++) {
@@ -589,7 +588,7 @@
 //     if (depth === 0 || result !== null){
 //       return result;
 //     }
-    
+
 //     if (isMaximizing) {
 //       let bestScore = -Infinity;
 //       let move;
@@ -638,7 +637,6 @@
 //       return bestScore;
 //     }
 //   };
-
 
 //   let cells, turns, p1Symbol, p2Symbol, p1, p2, isXsTurn, singlePGame,gameDone;
 //   const p1Form = document.querySelector(".p1-form");
@@ -984,39 +982,31 @@
 // //   };
 
 // //   init();
-  
 
 //   return{
 //     humanMove
 //   }
 // })();
 
-
-
-
-
-
-
-
-
-const gameBoard = (()=>{
-
+const gameBoard = (() => {
   let board;
   let row = 3;
 
   const initBoard = () => {
     board = [
-      ["","",""],
-      ["","",""],
-      ["","",""]
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
     ];
     renderBoard();
   };
 
-  const getCurrentBoard = ()=>{return board};
+  const getCurrentBoard = () => {
+    return board;
+  };
 
-  const appendToBoard = (row, col, symbol)=>{
-    board[row][col]=symbol
+  const appendToBoard = (row, col, symbol) => {
+    board[row][col] = symbol;
   };
 
   const renderBoard = () => {
@@ -1052,7 +1042,7 @@ const gameBoard = (()=>{
 
     svg.append(path);
     target.append(svg);
-};
+  };
 
   const oSvgMaker = (target) => {
     target.innerHTML = "";
@@ -1078,19 +1068,17 @@ const gameBoard = (()=>{
   const placeSymbol = (row, column, symbol) => {
     board[row][column] = symbol;
     renderBoard();
-    return [board];
+    // return [board];
   };
 
+  initBoard();
 
-  initBoard()
-  
-
-  return{
+  return {
     getCurrentBoard,
     appendToBoard,
     placeSymbol,
-    initBoard
-  }
+    initBoard,
+  };
 })();
 
 const PlayerFactory = (name, symbol) => {
@@ -1115,12 +1103,11 @@ const PlayerFactory = (name, symbol) => {
     setScore,
     getName,
     getScore,
-    getSymbol
+    getSymbol,
   };
 };
 
-const pageManager = (()=>{
-
+const pageManager = (() => {
   let singlePlayerGame;
 
   const p1Form = document.querySelector(".p1-form");
@@ -1135,54 +1122,53 @@ const pageManager = (()=>{
   const xRadio = document.querySelector(".x-radio");
   const oRadio = document.querySelector(".o-radio");
 
-
-  const newGame = ()=>{
+  const newGame = () => {
     document.querySelector(".p-vs-ai-container").classList.add("active");
     document.querySelector(".p1-form").classList.add("active");
-    bindEventListeners()
+    bindEventListeners();
   };
 
-  const bindEventListeners = ()=>{
-    document.querySelector(".p-vs-ai-header").addEventListener("click", showSinglePlayerForm);
-    document.querySelector(".p-vs-p-header").addEventListener("click", showMultiplePlayerForm);
+  const bindEventListeners = () => {
+    document
+      .querySelector(".p-vs-ai-header")
+      .addEventListener("click", showSinglePlayerForm);
+    document
+      .querySelector(".p-vs-p-header")
+      .addEventListener("click", showMultiplePlayerForm);
 
-    
     p1Form.addEventListener("submit", p1FormSubmission);
     p1Name.addEventListener("input", p1FluidValidity);
-    
+
     p2Form.addEventListener("submit", p2FormSubmission);
     p2Name.addEventListener("input", p2FluidValidity);
-    
+
     xRadio.addEventListener("input", radioFluidValidity);
     oRadio.addEventListener("input", radioFluidValidity);
-    
+
     singlePForm.addEventListener("submit", singlePlayerFormSubmission);
     singlePName.addEventListener("input", singlePFluidValidity);
-
-  }; 
-
-  const showSinglePlayerForm =()=>{
-    document.querySelector(".p-vs-ai-container").classList.add("active")
-    document.querySelector(".p-vs-p-container").classList.remove("active")
   };
 
-  const showMultiplePlayerForm =()=>{
-    document.querySelector(".p-vs-p-container").classList.add("active")
-    document.querySelector(".p-vs-ai-container").classList.remove("active")
-
+  const showSinglePlayerForm = () => {
+    document.querySelector(".p-vs-ai-container").classList.add("active");
+    document.querySelector(".p-vs-p-container").classList.remove("active");
   };
 
-  const hideStartScreen = ()=>{
+  const showMultiplePlayerForm = () => {
+    document.querySelector(".p-vs-p-container").classList.add("active");
+    document.querySelector(".p-vs-ai-container").classList.remove("active");
+  };
+
+  const hideStartScreen = () => {
     document.querySelector(".start-screen-container").classList.add("hide");
   };
 
-  const showBoard = ()=>{
+  const showBoard = () => {
     document.querySelector(".board-container").classList.add("active");
-    hideStartScreen()
+    hideStartScreen();
   };
 
-
-  const singlePlayerFormSubmission = (e)=>{
+  const singlePlayerFormSubmission = (e) => {
     e.preventDefault();
     let name;
     singlePlayerGame = true;
@@ -1191,20 +1177,18 @@ const pageManager = (()=>{
       document.querySelector(".single-p-name-error").textContent =
         "You must input a name.";
       return;
-    } 
-    
-    else { 
+    } else {
       pageManager.showBoard();
       name = singlePName.value;
       p1Symbol = "x";
-      singlePGame = true;
+      singlePlayerGame = true;
       p1 = PlayerFactory(name, p1Symbol);
       p2 = PlayerFactory("AI", "o");
-      startGame();
+      gameFlow.startSinglePlayerGame();
     }
   };
 
-  const p1FormSubmission = (e)=>{
+  const p1FormSubmission = (e) => {
     e.preventDefault();
     let name;
 
@@ -1217,21 +1201,20 @@ const pageManager = (()=>{
     if (!xRadio.checked && !oRadio.checked) {
       document.querySelector(".symbol-error").textContent =
         "You must select a symbol.";
-    } 
-    else {
+    } else {
       singlePlayerGame = false;
-     
+
       name = p1Name.value;
       p1Symbol = xRadio.checked ? "x" : "o";
       p1IsX = xRadio.checked ? true : false;
       p1 = PlayerFactory(name, p1Symbol);
-      
+
       p1Form.classList.remove("active");
       p2Form.classList.add("active");
     }
   };
 
-  const p2FormSubmission = (e)=>{
+  const p2FormSubmission = (e) => {
     if (!singlePlayerGame) {
       e.preventDefault();
       let name;
@@ -1244,17 +1227,16 @@ const pageManager = (()=>{
         name = p2Name.value;
         p2Symbol = p1.symbol === "x" ? "o" : "x";
         p2 = PlayerFactory(name, p2Symbol);
-        
+
         p2Form.classList.remove("active");
-        
+
         showBoard();
         startGame();
       }
     }
   };
 
-
-    //////////////////Fluid Form Validation///////////////////////////
+  //////////////////Fluid Form Validation///////////////////////////
   const p1FluidValidity = () => {
     if (p1Name.validity.valid) {
       document.querySelector(".p1-name-error").textContent = "";
@@ -1279,47 +1261,369 @@ const pageManager = (()=>{
     }
   };
 
-
-
-
-
   newGame();
 
-  return{
+  return {
     newGame,
-    showBoard
-  }
+    showBoard,
+  };
 })();
 
-//////////////Game Flow////////////////////////////
-const gameFlow = (()=>{
+const aiModule = (() => {
+  let bestMove;
+  const ai = "o";
+  const human = "x";
 
-  let singlePlayerGame;
-
-  const initNewGame = ()=>{
-    gameBoard.initBoard()
-    bindEventListeners()
+  const getBestMove = () => {
+    return bestMove;
   };
 
-const bindEventListeners=()=>{
+  const evaluate = (board) => {
+    const winningCombos = [
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+      ], //rows
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+      [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ],
+      [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+      ], //col
+      [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
+      [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+      ],
+      [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+      ], //diagonal
+      [
+        [0, 2],
+        [1, 1],
+        [2, 0],
+      ],
+    ];
 
-};
+    // Check for a winner
+    for (let combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (
+        board[a[0]][a[1]] !== "" &&
+        board[a[0]][a[1]] === board[b[0]][b[1]] &&
+        board[a[0]][a[1]] === board[c[0]][c[1]]
+      ) {
+        if (board[a[0]][a[1]] === human) return -1; // Human wins
+        else return 1; // AI wins
+      }
+    }
 
+    // Game is a draw
+    if (isBoardFull(board)) return 0;
 
-
-
-const multiplePlayers = (e)=>{
-  e.preventDefault();
-  singlePlayerGame = false;
-  pageManager.showBoard();
-};
-
-
-  const startNewGame =()=>{
-
+    // No winner yet
+    return null;
   };
 
-  initNewGame()
+  // Check if the board is full
+  const isBoardFull = (board) => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === "") return false;
+      }
+    }
+    return true;
+  };
+
+  const minimax = (board, depth, isMaximizing) => {
+    let result = evaluate(board);
+    // Base cases
+    if (depth === 0 || result !== null) {
+      return result;
+    }
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      let move;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Check empty spots
+          if (board[i][j] === "") {
+            // Simulate a move
+            board[i][j] = ai;
+            // Call minimax recursively
+            let score = minimax(board, depth - 1, false);
+            // Undo move
+            board[i][j] = "";
+            // Update bestScore and move if necessary
+            if (score > bestScore) {
+              bestScore = score;
+              move = [i, j];
+            }
+          }
+        }
+      }
+      bestMove = move;
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      let move;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Check empty spots
+          if (board[i][j] === "") {
+            // Simulate a move
+            board[i][j] = human;
+            // Call minimax recursively
+            let score = minimax(board, depth - 1, true);
+            // Undo move
+            board[i][j] = "";
+            // Update bestScore and move if necessary
+            if (score < bestScore) {
+              bestScore = score;
+              move = [i, j];
+            }
+          }
+        }
+      }
+      bestMove = move;
+      return bestScore;
+    }
+  };
+
+  return {
+    evaluate,
+    isBoardFull,
+    minimax,
+    getBestMove,
+  };
+})();
+
+//----------------Game Flow--------------------//
+const gameFlow = (() => {
+  let singlePlayerGame, cells, isXsTurn, board;
+  let turns = 0;
+  const human = "x";
+  const ai = "o";
+
+  const initNewGame = () => {
+    gameBoard.initBoard();
+    cells = Array.from(document.querySelectorAll(".cell"));
+    console.log(cells);
+    board = gameBoard.getCurrentBoard();
+    bindEventListeners();
+  };
+
+  const bindEventListeners = () => {};
+
+  //----------------Single Player Game--------------------//
+  const bindSingleGameCellEvents = () => {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", singlePlayerTurn);
+    });
+  };
+
+  const startSinglePlayerGame = () => {
+    bindSingleGameCellEvents();
+    isXsTurn = true;
+  };
+
+  const singlePlayerTurn = (e) => {
+    singlePlayerGame = true;
+    let currentRow = Number(e.currentTarget.getAttribute("data-row"));
+    let currentColumn = Number(e.currentTarget.getAttribute("data-column"));
+    board = gameBoard.getCurrentBoard();
+    console.log(board);
+
+    // Makes sure cell is empty.
+    if (board[currentRow][currentColumn] === "") {
+      turns++;
+
+      gameBoard.placeSymbol(currentRow, currentColumn, human);
+      checkGame(currentRow, currentColumn, human);
+      console.log(turns)
+      aiTurn();
+    }
+  };
+
+  const aiTurn = () => {
+    if (turns<9){
+      board = gameBoard.getCurrentBoard();
+      aiModule.minimax(board,9, true);
+      let bestMove = aiModule.getBestMove();
+      gameBoard.appendToBoard(bestMove[0], bestMove[1], ai);
+      gameBoard.placeSymbol(bestMove[0], bestMove[1], ai);
+      turns++
+      // debugger
+      console.log(turns)
+      checkGame(bestMove[0], bestMove[1], ai);
+    }
+  };
+
+  //------------------Check Game------------------//
+  const checkGame = (row, column, symbol) => {
+    if (turns > 4) {
+      debugger
+      let checkRowVar = checkRow(row, symbol);
+      let checkColumnVar = checkColumn(column, symbol);
+      let checkDiagonalVar = false;
+      if (row === column || row + column === 2) {
+        checkDiagonal(symbol);
+      }
+      if (turns === 9 && !checkRowVar && !checkColumnVar && !checkDiagonalVar){
+        winner("tie")
+      }
+    }
+    
+  };
+
+
+
+  //Individual Checkers
+  const checkRow = (row, symbol) => {
+    // Row loop
+    if (
+      board[row][0] !== "" &&
+      board[row][0] === board[row][1] &&
+      board[row][0] === board[row][2]
+    ) {
+      winner(symbol);
+      return true
+    }else {return false}
+  };
+
+  //Individual Checkers
+  const checkColumn = (column, symbol) => {
+    if (
+      board[0][column] !== "" &&
+      board[0][column] === board[1][column] &&
+      board[0][column] === board[2][column]
+    ) {
+      winner(symbol);
+      return true
+    }else {return false}
+  };
+
+  //Individual Checkers
+  const checkDiagonal = (symbol) => {
+    if (
+      board[0][0] !== "" &&
+      board[0][0] === board[1][1] &&
+      board[0][0] === board[2][2]
+    ) {
+      winner(symbol);
+      return true
+    }
+
+    else if (
+      board[0][2] !== "" &&
+      board[0][2] === board[1][1] &&
+      board[0][2] === board[2][0]
+    ) {
+      winner(symbol);
+      return true
+    }
+    else {return false}
+  };
+
+  // debugger
+  const winner = (symbol) => {
+    // removeEvents();
+    // gameDone = true;
+
+    if (singlePlayerGame) {
+      if (symbol === "x") {
+        p1.setScore();
+        displayWinner(p1, p2);
+      } else if (symbol === "o") {
+        p2.setScore();
+        displayWinner(p2, p1);
+      } else {
+        displayWinner("tie", "tie");
+      }
+    } else {
+      if (symbol === "x") {
+        if (p1IsX) {
+          console.log("Player One is the winner");
+          // Must be before displayWinner().
+          p1.setScore();
+          displayWinner(p1, p2);
+        } else {
+          console.log("Player Two is the winner");
+          p2.setScore();
+          displayWinner(p2, p1);
+        }
+      } else {
+        if (p1Symbol === "o") {
+          console.log("Player One is the winner");
+          p1.setScore();
+          displayWinner(p1, p2);
+        } else {
+          console.log("Player Two is the winner");
+          p2.setScore();
+          displayWinner(p2, p1);
+        }
+      }
+    }
+  };
+
+  const displayWinner = (winner, loser) => {
+    winnerName = p1.getName();
+    loserName = p2.getName();
+    wScore = p1.getScore();
+    lScore = p2.getScore();
+    
+
+    if (winner === "tie") {
+      document.querySelector(".winner-msg").textContent = "It's A Tie";
+
+      document.querySelector(
+        ".score-update"
+      ).textContent = `The score is ${wScore} for ${winnerName} and ${lScore} for ${loserName} !!`;
+
+      document.querySelector(".end-page").classList.add("active");
+
+    } else {
+      winnerName = winner.getName();
+      loserName = loser.getName();
+      wScore = winner.getScore();
+      lScore = loser.getScore();
+
+      document.querySelector(
+        ".winner-msg"
+      ).textContent = `${winnerName} is the winner!!`;
+      document.querySelector(
+        ".score-update"
+      ).textContent = `The score is ${wScore} for ${winnerName} and ${lScore} for ${loserName} !!`;
+      
+      document.querySelector(".end-page").classList.add("active");
+    }
+  };
+
+  const startNewGame = () => {};
+
+  initNewGame();
+
+  return {
+    startSinglePlayerGame,
+  };
 })();
 
 // const =()=>{};
